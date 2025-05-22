10dd775afe529abca7e1756feb93ec693a515e01
Winterson <u19cse1028@cit.ac.in>
2025-05-23 00:31:10 +0530

    modify: add auth pages to auth route group

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import SignIn from "@/src/components/pages/auth/SignIn";
+
+function SignInPage() {
+	return (
+		<main>
+			<SignIn />
+		</main>
+	);
+}
+
+export default SignInPage;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import SignUp from "@/src/components/pages/auth/SignUp";
+
+function SignUpPage() {
+	return (
+		<main>
+			<SignUp />
+		</main>
+	);
+}
+
+export default SignUp;
