7e2678148f602c1744706bc5d2295ccad461489b
Winterson <u19cse1028@cit.ac.in>
2025-05-26 14:15:24 +0530

    modify: for rpc support

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,11 +1,11 @@
+import auth from "@/src/components/features/server/auth/route";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
 const app = new Hono().basePath("/api");
 
-app.get("/echo/:message", (c) => {
-	const { message } = c.req.param();
-	return c.json({ message: message });
-});
+const routes = app.route("/auth", auth);
 
 export const GET = handle(app);
+
+export type AppType = typeof routes;
