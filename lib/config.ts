c601eb1e76e6e86a19ec793bbddda49f40917ef4
Winterson <u19cse1028@cit.ac.in>
2025-05-26 23:41:10 +0530

    modify: config.ts file to not dynamically read in env variables as they work differently in server compared to client side

--- lib/config.ts
+++ lib/config.ts
@@ -2,32 +2,18 @@ interface EnvConfig {
 	NEXT_PUBLIC_API_URL: string;
 }
 
-function getEnvVariable(key: keyof EnvConfig, defaultValue?: string) {
-	const value = process.env[key];
-	if (value === undefined && defaultValue === undefined) {
-		throw new Error(`Environment variable ${key} is not set`);
-	}
-	// biome-ignore lint/style/noNonNullAssertion: < If execution reaches this line, it confirms that value or defaultValue holds a valid assignment >
-	return value ?? defaultValue!;
-}
-
 export const ENV: Readonly<EnvConfig> = {
-	NEXT_PUBLIC_API_URL: getEnvVariable(
-		"NEXT_PUBLIC_API_URL",
-		"http://localhost:3001",
-	),
-};
+	NEXT_PUBLIC_API_URL:
+		process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3001",
+} as const;
 
-//? For Development Server
+type EnvKeys = keyof typeof ENV;
+
 if (process.env.NODE_ENV === "development") {
 	for (const key in ENV) {
-		if (
-			ENV[key as keyof EnvConfig] === undefined ||
-			ENV[key as keyof EnvConfig] === null
-		) {
-			console.log(
-				`Warning: Environment variable "${key} is not set in .env.local or has an invalid value"`,
-			);
+		const val = ENV[key as EnvKeys];
+		if (!val || val === "") {
+			console.warn(`Warning: Env variable ${key} is missing or empty`);
 		}
 	}
 }
