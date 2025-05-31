3527ecbda5c9df65bf18e91cee3cdbcd0e0b65f4
Winterson <u19cse1028@cit.ac.in>
2025-05-31 16:44:23 +0530

    modify: home page (incomplete)

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,13 +1,23 @@
+"use client";
 import Link from "next/link";
+import { useLogout } from "../../features/api/auth/useLogout";
 import { Button } from "../../ui/button";
 
 function Home() {
+	const { mutate } = useLogout();
+	const onClickLogout = () => {
+		mutate();
+	};
 	return (
 		<main>
 			<h1>This is the Home Page</h1>
 			<div>
-				<Button asChild className="hover:cursor-pointer">
-					<Link href={"/sign-in"}>Sign In</Link>
+				<Button
+					asChild
+					className="hover:cursor-pointer"
+					onClick={onClickLogout}
+				>
+					Logout
 				</Button>
 			</div>
 		</main>
