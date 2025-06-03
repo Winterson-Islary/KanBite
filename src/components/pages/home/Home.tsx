21f618842dd525b850d764b5d2a96301ec67fa38
Winterson <u19cse1028@cit.ac.in>
2025-06-03 23:46:24 +0530

    modify: add redirect for unauthorized users using next redirect

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,7 +1,10 @@
-"use client";
+import { redirect } from "next/navigation";
+import { getCurrentUser } from "../../features/server/auth/actions";
 import UserButton from "../../features/ui/user-button";
 
-function Home() {
+async function Home() {
+	const user = await getCurrentUser();
+	if (!user) redirect("/sign-in");
 	return (
 		<main>
 			<h1>This is the Home Page</h1>
