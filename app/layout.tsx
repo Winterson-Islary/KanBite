ef2998d1c5f65f14e46b8b74051d396fd23c6901
Winterson <u19cse1028@cit.ac.in>
2025-06-05 23:27:02 +0530

    modify: move navbar components, and layout code to (dashboard) page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/src/styles/globals.css";
-import Navbar from "@/src/components/features/ui/navbar";
 import { QueryProvider } from "@/src/components/providers/queryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,14 +19,8 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={cn(inter.className, "antialiased")}>
-				<main className="min-h-screen  max-w-screen-2xl mx-auto grid [grid-template-rows:auto_1fr_auto] px-[2rem] md:px-[5rem] lg:px-[10rem]">
-					<header>
-						<Navbar />
-					</header>
-					<article>
-						<QueryProvider>{children}</QueryProvider>
-					</article>
-					<footer>Footer</footer>
+				<main>
+					<QueryProvider>{children}</QueryProvider>
 				</main>
 			</body>
 		</html>
