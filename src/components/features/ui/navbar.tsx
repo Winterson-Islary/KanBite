3e6ab2fac11b15f88fce474a2d1a685d42dce26d
Winterson <u19cse1028@cit.ac.in>
2025-06-05 23:27:41 +0530

    modify: add UserButton to navbar instead of login/signup button

--- src/components/features/ui/navbar.tsx
+++ src/components/features/ui/navbar.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { Button } from "@/src/components/ui/button";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import UserButton from "./user-button";
 
 const NavbarLinks = ["dashboard", "settings"];
 function GetNavLinks(links: string[]) {
@@ -17,7 +16,6 @@ function GetNavLinks(links: string[]) {
 	);
 }
 function Navbar() {
-	const pathname = usePathname();
 	return (
 		<main className="py-2 flex items-center justify-between">
 			<section>
@@ -27,15 +25,7 @@ function Navbar() {
 			</section>
 			<section>{GetNavLinks(NavbarLinks)}</section>
 			<section>
-				<Button
-					variant={"ghost"}
-					asChild
-					className="min-w-[100px] border-[1px] border-slate-300"
-				>
-					<Link href={pathname === "/sign-in" ? "/sign-up" : "/sign-in"}>
-						{pathname === "/sign-in" ? "Sign up" : "Login"}
-					</Link>
-				</Button>
+				<UserButton />
 			</section>
 		</main>
 	);
