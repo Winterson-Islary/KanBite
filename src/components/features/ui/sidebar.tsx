cca15cb84509f715433e6ecb0ccd19083a8309f8
Winterson <u19cse1028@cit.ac.in>
2025-06-08 00:42:55 +0530

    modify: add page descriptions to the navigationRoutues Object[]

--- src/components/features/ui/sidebar.tsx
+++ src/components/features/ui/sidebar.tsx
@@ -11,51 +11,58 @@ import {
 export default function Sidebar() {
 	return (
 		<article className="h-full">
-			<main className="h-full lg:pt-12">
+			<main className="h-full p-2.5 flex-col gap-2 bg-neutral-100">
+				<header className="pb-3">
+					<h1 className="text-4xl font-extralight">KANBITE</h1>
+				</header>
 				<Navigation />
 			</main>
 		</article>
 	);
 }
 
-const routes = [
+export const NavigationRoutes = [
 	{
 		label: "Home",
-		href: "",
+		href: "/",
 		icon: GoHome,
 		activeIcon: GoHomeFill,
+		desc: "Monitor all of your projects and tasks here",
 	},
 	{
 		label: "My Tasks",
 		href: "/tasks",
 		icon: GoCheckCircle,
 		activeIcon: GoCheckCircleFill,
+		desc: "",
 	},
 	{
 		label: "Settings",
 		href: "/settings",
 		icon: SettingsIcon,
 		activeIcon: SettingsIcon,
+		desc: "",
 	},
 	{
 		label: "Members",
 		href: "/members",
 		icon: UsersIcon,
 		activeIcon: UsersIcon,
+		desc: "",
 	},
 ];
 
 function Navigation() {
 	return (
 		<ul>
-			{routes.map((route) => {
+			{NavigationRoutes.map((route) => {
 				const isActive = false;
 				const Icon = isActive ? route.activeIcon : route.icon;
 				return (
 					<Link key={route.href} href={route.href}>
 						<main
 							className={cn(
-								"flex items-center gap-2.5 p-2.5 rounded-md font-normal hover:text-primary transition text-neutral-500",
+								"flex items-center gap-2.5 py-2.5 rounded-md font-normal hover:text-primary transition text-neutral-500",
 								isActive && "bg-white shadow-sm hover:opacity-100 text-primary",
 							)}
 						>
