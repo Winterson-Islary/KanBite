efc2fe7ab389873f2b6df5b6041cbeaf067542da
Winterson <u19cse1028@cit.ac.in>
2025-06-08 00:41:48 +0530

    modify: add description headers describing the page and its contents

--- src/components/features/ui/navbar.tsx
+++ src/components/features/ui/navbar.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import MobileSidebar from "./mobile-sidebar";
+import { NavigationRoutes } from "./sidebar";
 import UserButton from "./user-button";
 
 const NavbarLinks = ["dashboard", "settings"];
@@ -17,14 +19,24 @@ function GetNavLinks(links: string[]) {
 	);
 }
 function Navbar() {
+	const pathname = usePathname();
+	const pageDescription = NavigationRoutes.find(
+		(item) => item.href === pathname,
+	);
 	return (
-		<main className="py-2 flex items-center justify-between">
-			<section>
+		<main className="py-2 flex items-center justify-between w-full">
+			<section className="block lg:hidden">
 				<MobileSidebar />
 			</section>
 			{/* <section>{GetNavLinks(NavbarLinks)}</section> */}
-			<section>
-				<UserButton />
+			<section className="flex items-center justify-end lg:justify-between w-full">
+				<section id="left-side" className="hidden lg:block">
+					<h1 className="text-3xl font-light">{pageDescription?.label}</h1>
+					<h2 className="text-muted-foreground">{pageDescription?.desc}</h2>
+				</section>
+				<section id="right-side" className="">
+					<UserButton />
+				</section>
 			</section>
 		</main>
 	);
