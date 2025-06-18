083bf864b8bd26e22d74d576407fd55823cf072c
Winterson <u19cse1028@cit.ac.in>
2025-06-18 23:46:27 +0530

    modify: add GET method to fetch user workspaces

--- src/components/features/server/workspaces/route.ts
+++ src/components/features/server/workspaces/route.ts
@@ -5,40 +5,49 @@ import { ID } from "node-appwrite";
 import { sessionMiddleware } from "../../middlewares/session-middleware";
 import { createWorkspaceSchema } from "./schemas/workspaces-schema";
 
-const app = new Hono().post(
-	"/",
-	zValidator("json", createWorkspaceSchema),
-	sessionMiddleware,
-	async (c) => {
+const app = new Hono()
+	.get("/", sessionMiddleware, async (c) => {
 		const database = c.get("databases");
-		const storage = c.get("storage");
-		const user = c.get("user");
-		const { name, image } = c.req.valid("json");
-		let uploadedImageUrl: string | undefined;
-		if (image instanceof File) {
-			const file = await storage.createFile(
-				ENV.NEXT_PUBLIC_APPWRITE_BUCKET_ID,
-				ID.unique(),
-				image,
-			);
-			const arrayBuffer = await storage.getFilePreview(
-				ENV.NEXT_PUBLIC_APPWRITE_BUCKET_ID,
-				file.$id,
-			);
-			uploadedImageUrl = `data:image/png;base64, ${Buffer.from(arrayBuffer).toString("base64")}`;
-		}
-		const workspace = await database.createDocument(
+		const workspaces = await database.listDocuments(
 			ENV.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
 			ENV.NEXT_PUBLIC_APPWRITE_WORKSPACES_ID,
-			ID.unique(),
-			{
-				name,
-				userId: user.$id,
-				imageUrl: uploadedImageUrl,
-			},
 		);
-		return c.json({ data: workspace });
-	},
-);
+		return c.json({ data: workspaces });
+	})
+	.post(
+		"/",
+		zValidator("form", createWorkspaceSchema),
+		sessionMiddleware,
+		async (c) => {
+			const database = c.get("databases");
+			const storage = c.get("storage");
+			const user = c.get("user");
+			const { name, image } = c.req.valid("form");
+			let uploadedImageUrl: string | undefined;
+			if (image instanceof File) {
+				const file = await storage.createFile(
+					ENV.NEXT_PUBLIC_APPWRITE_BUCKET_ID,
+					ID.unique(),
+					image,
+				);
+				const arrayBuffer = await storage.getFilePreview(
+					ENV.NEXT_PUBLIC_APPWRITE_BUCKET_ID,
+					file.$id,
+				);
+				uploadedImageUrl = `data:image/png;base64, ${Buffer.from(arrayBuffer).toString("base64")}`;
+			}
+			const workspace = await database.createDocument(
+				ENV.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
+				ENV.NEXT_PUBLIC_APPWRITE_WORKSPACES_ID,
+				ID.unique(),
+				{
+					name,
+					userId: user.$id,
+					imageUrl: uploadedImageUrl,
+				},
+			);
+			return c.json({ data: workspace });
+		},
+	);
 
 export default app;
