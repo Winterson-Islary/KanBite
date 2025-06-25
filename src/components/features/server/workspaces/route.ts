e3c20f7065f74c32e96a7cf44a79ec8635b062be
Winterson <u19cse1028@cit.ac.in>
2025-06-25 23:53:20 +0530

    modify: add workspace admin everytime and user creates a new workspace

--- src/components/features/server/workspaces/route.ts
+++ src/components/features/server/workspaces/route.ts
@@ -1,16 +1,31 @@
 import { ENV } from "@/lib/config";
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { sessionMiddleware } from "../../middlewares/session-middleware";
+import { MEMBER_ROLE } from "../members/constants/types";
 import { createWorkspaceSchema } from "./schemas/workspaces-schema";
 
 const app = new Hono()
 	.get("/", sessionMiddleware, async (c) => {
+		const current_user = c.get("user");
 		const database = c.get("databases");
+		const memberOfWorkspaces = await database.listDocuments(
+			ENV.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
+			ENV.NEXT_PUBLIC_APPWRITE_MEMBERS_ID,
+			[Query.equal("userId", current_user.$id)],
+		);
+		if (memberOfWorkspaces.total === 0) {
+			return c.json({ data: { documents: [], total: 0 } });
+		}
+		const workspaceIds = memberOfWorkspaces.documents.map(
+			(member) => member.workspaceId,
+		);
+
 		const workspaces = await database.listDocuments(
 			ENV.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
 			ENV.NEXT_PUBLIC_APPWRITE_WORKSPACES_ID,
+			[Query.orderDesc("$createdAt"), Query.contains("$id", workspaceIds)],
 		);
 		return c.json({ data: workspaces });
 	})
@@ -44,6 +59,16 @@ const app = new Hono()
 					name,
 					userId: user.$id,
 					imageUrl: uploadedImageUrl,
+				},
+			);
+			await database.createDocument(
+				ENV.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
+				ENV.NEXT_PUBLIC_APPWRITE_MEMBERS_ID,
+				ID.unique(),
+				{
+					userId: user.$id,
+					workspaceId: workspace.$id,
+					role: MEMBER_ROLE.ADMIN,
 				},
 			);
 			return c.json({ data: workspace });
