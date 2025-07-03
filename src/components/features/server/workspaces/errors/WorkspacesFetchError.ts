4d4620f0b1cd83e59c14962da750c526a3c2eed9
Winterson <u19cse1028@cit.ac.in>
2025-07-03 23:35:42 +0530

    modify: add WorkspacesFetchError custom error class

--- src/components/features/server/workspaces/errors/WorkspacesFetchError.ts
+++ src/components/features/server/workspaces/errors/WorkspacesFetchError.ts
@@ -3,3 +3,19 @@ export class WorkspacesFetchError extends Error {
 		super("Failed to fetch workspaces");
 	}
 }
+
+export class UnauthorizedError extends Error {
+	public readonly statusCode: number;
+
+	constructor(message = "Unauthorized access.") {
+		super(message);
+		this.name = "UnauthorizedError"; // Good practice to set the name
+		this.statusCode = 401; // Common HTTP status code for Unauthorized
+	}
+}
+
+export class WorkspaceUpdateError extends Error {
+	constructor() {
+		super("Failed to update workspaces");
+	}
+}
