ce82c3d72a5845b37324adcfe420b09d66252540
Winterson <u19cse1028@cit.ac.in>
2025-07-03 23:33:19 +0530

    modify: add getWorkspaceById method to fetch workspace info to be prefill workspace update form

--- src/components/features/server/workspaces/actions.ts
+++ src/components/features/server/workspaces/actions.ts
@@ -3,6 +3,8 @@ import { ENV } from "@/lib/config";
 import { AUTH_COOKIE } from "@/src/components/features/server/auth/constants";
 import { cookies } from "next/headers";
 import { Account, Client, Databases, Query } from "node-appwrite";
+import { getMember } from "../members/utils/getMember";
+import type { Workspace } from "./types/update-workspace-form";
 
 export const getUserWorkspaces = async () => {
 	try {
@@ -36,5 +38,39 @@ export const getUserWorkspaces = async () => {
 		return workspaces;
 	} catch {
 		return { documents: [], total: 0 };
+	}
+};
+
+type GetWorkspaceProps = {
+	workspaceId: string;
+};
+export const getWorkspaceById = async ({ workspaceId }: GetWorkspaceProps) => {
+	try {
+		const client = new Client()
+			.setEndpoint(ENV.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+			.setProject(ENV.NEXT_PUBLIC_APPWRITE_PROJECT);
+		const cookie = await cookies();
+		const session = cookie.get(AUTH_COOKIE);
+		if (!session) return null;
+		client.setSession(session.value);
+		const account = new Account(client);
+		const current_user = await account.get();
+		const database = new Databases(client);
+
+		const member = getMember({
+			databases: database,
+			userId: current_user.$id,
+			workspaceId,
+		});
+		if (!member) return null;
+
+		const workspace = await database.getDocument<Workspace>(
+			ENV.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
+			ENV.NEXT_PUBLIC_APPWRITE_WORKSPACES_ID,
+			workspaceId,
+		);
+		return workspace;
+	} catch {
+		return null;
 	}
 };
