d57340e03d4f31933320c9e8910fd9eba9af4c09
Winterson <u19cse1028@cit.ac.in>
2025-07-03 23:36:28 +0530

    modify: add patch method, to update workspaces

--- src/components/features/server/workspaces/route.ts
+++ src/components/features/server/workspaces/route.ts
@@ -5,7 +5,11 @@ import { Hono } from "hono";
 import { ID, Query } from "node-appwrite";
 import { sessionMiddleware } from "../../middlewares/session-middleware";
 import { MEMBER_ROLE } from "../members/constants/types";
-import { createWorkspaceSchema } from "./schemas/workspaces-schema";
+import { getMember } from "../members/utils/getMember";
+import {
+	createWorkspaceSchema,
+	updateWorkspaceSchema,
+} from "./schemas/workspaces-schema";
 
 const app = new Hono()
 	.get("/", sessionMiddleware, async (c) => {
@@ -73,6 +77,53 @@ const app = new Hono()
 					role: MEMBER_ROLE.ADMIN,
 				},
 			);
+			return c.json({ data: workspace });
+		},
+	)
+	.patch(
+		"/:workspaceId",
+		sessionMiddleware,
+		zValidator("form", updateWorkspaceSchema),
+		async (c) => {
+			const databases = c.get("databases");
+			const storage = c.get("storage");
+			const user = c.get("user");
+			const { workspaceId } = c.req.param();
+			const { name, image } = c.req.valid("form");
+			const member = await getMember({
+				databases,
+				workspaceId,
+				userId: user.$id,
+			});
+			if (!member || member.role !== MEMBER_ROLE.ADMIN) {
+				return c.json({ error: "Unauthorized" }, 401);
+			}
+			let uploadedImageUrl: string | undefined;
+			if (image instanceof File) {
+				const file = await storage.createFile(
+					ENV.NEXT_PUBLIC_APPWRITE_BUCKET_ID,
+					ID.unique(),
+					image,
+				);
+				const arrayBuffer = await storage.getFilePreview(
+					ENV.NEXT_PUBLIC_APPWRITE_BUCKET_ID,
+					file.$id,
+				);
+				uploadedImageUrl = `data:image/png;base64, ${Buffer.from(arrayBuffer).toString("base64")}`;
+			} else {
+				uploadedImageUrl = image;
+			}
+
+			const workspace = await databases.updateDocument(
+				ENV.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
+				ENV.NEXT_PUBLIC_APPWRITE_WORKSPACES_ID,
+				workspaceId,
+				{
+					name,
+					imageUrl: uploadedImageUrl,
+				},
+			);
+
 			return c.json({ data: workspace });
 		},
 	);
