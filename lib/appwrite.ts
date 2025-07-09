f4c3099aa3f646e12ad57d6d5172df3b1a0b38f9
Winterson <u19cse1028@cit.ac.in>
2025-07-10 02:28:23 +0530

    modify: add method to create client session

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,6 @@
 import "server-only";
+import { AUTH_COOKIE } from "@/src/components/features/server/auth/constants";
+import { cookies } from "next/headers";
 import { Account, Client, Databases, Storage, Users } from "node-appwrite";
 import { ENV } from "./config";
 
@@ -11,6 +13,25 @@ export async function createAdminClient() {
 	return {
 		get account() {
 			return new Account(client);
+		},
+	};
+}
+
+export async function createSessionClient() {
+	const client = new Client()
+		.setEndpoint(ENV.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+		.setProject(ENV.NEXT_PUBLIC_APPWRITE_PROJECT);
+	const cookie = await cookies();
+	const session = cookie.get(AUTH_COOKIE);
+	if (!session || !session.value) throw new Error("Unauthorized");
+	client.setSession(session.value);
+
+	return {
+		get account() {
+			return new Account(client);
+		},
+		get databases() {
+			return new Databases(client);
 		},
 	};
 }
