f4297a68d24b615e1d02ac9403bf9922e2cf28f9
Winterson <u19cse1028@cit.ac.in>
2025-07-23 02:41:21 +0530

    add: join workspace page

---
+++ app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
@@ -0,0 +1,8 @@
+type WorkspaceIdJoinPageProps = {
+	params: { workspaceId: string };
+};
+function WorkspaceIdJoinPage({ params }: WorkspaceIdJoinPageProps) {
+	return <div>WorkspaceIdJoinPage</div>;
+}
+
+export default WorkspaceIdJoinPage;
