2f702106a4e5e1a4764f2fa1d074005e303995c9
Winterson <u19cse1028@cit.ac.in>
2025-07-23 02:45:29 +0530

    modify: add auth checks

--- app/(standalone)/layout.tsx
+++ app/(standalone)/layout.tsx
@@ -1,10 +1,14 @@
+import { getCurrentUser } from "@/src/components/features/server/auth/queries";
 import UserButton from "@/src/components/features/ui/user-button";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 type StandaloneLayoutProps = {
 	children: React.ReactNode;
 };
-function StandaloneLayout({ children }: StandaloneLayoutProps) {
+async function StandaloneLayout({ children }: StandaloneLayoutProps) {
+	const user = await getCurrentUser();
+	if (!user) redirect("/sign-in");
 	return (
 		<article>
 			<section className="max-w-screen-2xl mx-auto w-full min-h-screen p-2.5 grid [grid-template-rows:auto_1fr]">
