c0fa6a4d527c160896be29c3eaa4b8d9ab96370a
Winterson <u19cse1028@cit.ac.in>
2025-08-05 23:35:36 +0530

    add: members page inside of the standalone route

---
+++ app/(standalone)/workspaces/[workspaceId]/members/page.tsx
@@ -0,0 +1,11 @@
+import MembersList from "@/src/components/features/server/members/components/members-list";
+
+function WorkspaceIdMembersPage() {
+	return (
+		<main className="w-full lg:max-w-xl mx-auto">
+			<MembersList />
+		</main>
+	);
+}
+
+export default WorkspaceIdMembersPage;
