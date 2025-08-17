037450431e91f454ec622c332b04e21d73c13707
Winterson <u19cse1028@cit.ac.in>
2025-08-17 22:14:25 +0530

    modify: made stylistic changes to project avatar on the sidebar

--- src/components/features/ui/projects.tsx
+++ src/components/features/ui/projects.tsx
@@ -4,35 +4,35 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { RiAddCircleFill } from "react-icons/ri";
 import { useGetProjects } from "../server/projects/api/use-get-projects";
+import ProjectAvatar from "../server/projects/components/project-avatar";
 import { useCreateProjectModal } from "../server/projects/hooks/use-create-project-modal";
-import { useProjectId } from "../server/projects/hooks/use-project-id";
 import { useWorkspaceId } from "../server/workspaces/hooks/useWorkspaceId";
 function Projects() {
-	const projectId = useProjectId();
 	const { open } = useCreateProjectModal();
 	const pathname = usePathname();
 	const workspaceId = useWorkspaceId();
 	const { data } = useGetProjects({ workspaceId });
 	return (
 		<main className="flex flex-col gap-y-2">
-			<article className="flex justify-between items-center">
-				<p className="uppercase font-light text-black">Projects</p>
+			<article className="flex items-center justify-between">
+				<p className="font-light text-black uppercase">Projects</p>
 				<RiAddCircleFill
 					onClick={open}
-					className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition"
+					className="size-5 cursor-pointer text-neutral-500 transition hover:opacity-75"
 				/>
 			</article>
 			{data?.documents.map((project) => {
-				const href = `/workspaces/${workspaceId}/projects/${projectId}`;
+				const href = `/workspaces/${workspaceId}/projects/${project.$id}`;
 				const isActive = pathname === href;
 				return (
 					<Link href={href} key={project.$id}>
 						<div
 							className={cn(
-								"flex items-center gap-2.5 p-2.5 rounded-md hover:opacity-75 transition cursor-pointer text-neutral-500",
-								isActive && "bg-white shadow-sm hover:opacity-100 text-black",
+								"flex cursor-pointer items-center gap-2.5 rounded-md p-2.5 text-neutral-500 transition hover:opacity-75",
+								isActive && "bg-white text-black shadow-sm hover:opacity-100",
 							)}
 						>
+							<ProjectAvatar image={project.imageUrl} name={project.name} />
 							<span className="truncate">{project.name}</span>
 						</div>
 					</Link>
