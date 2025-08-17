f3bbf5b295d9b34cbade635003148f6a328031f4
Winterson <u19cse1028@cit.ac.in>
2025-08-18 01:46:58 +0530

    modify: add project avatar and edit-project button on Project page

--- app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -1,16 +1,39 @@
+import ProjectAvatar from "@/src/components/features/server/projects/components/project-avatar";
 import { getUserProject } from "@/src/components/features/server/projects/queries";
+import { Button } from "@/src/components/ui/button";
+import { PencilIcon } from "lucide-react";
+import Link from "next/link";
 
 type ProjectIdPageProps = {
 	params: { projectId: string };
 };
 
 async function ProjectIdPage({ params }: ProjectIdPageProps) {
-	const { projectId } = params;
+	const { projectId } = await params;
 	const initialValues = await getUserProject({ projectId });
+	//! Handle condition for missing initial-values
+	if (!initialValues) throw new Error("Project not found"); //? Throwing for now
+
 	return (
-		<main>
-			<h1>Project ID: {projectId}</h1>
-			<section id="data">{JSON.stringify(initialValues)}</section>
+		<main className="flex flex-col gap-y-4">
+			<div className="flex items-center justify-between">
+				<div className="flex items-center gap-x-2">
+					<ProjectAvatar
+						name={initialValues.name}
+						image={initialValues.imageUrl}
+						className="size-8"
+					/>
+					<p className="font-semibold text-lg">{initialValues.name}</p>
+				</div>
+				<div>
+					<Button variant="outline" asChild>
+						<Link href="">
+							<PencilIcon className=" size-4" />
+							Edit Project
+						</Link>
+					</Button>
+				</div>
+			</div>
 		</main>
 	);
 }
