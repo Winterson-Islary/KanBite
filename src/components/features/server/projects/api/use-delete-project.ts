a94cfd93a5d2d1f4df3be2a616c8063c6a757879
Winterson <u19cse1028@cit.ac.in>
2025-08-19 20:15:32 +0530

    add: api to delete a project

---
+++ src/components/features/server/projects/api/use-delete-project.ts
@@ -0,0 +1,38 @@
+import { client } from "@/lib/rpc";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { InferRequestType, InferResponseType } from "hono";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { ProjectDeleteError } from "../errors/delete-project-error";
+
+type ResponseType = InferResponseType<
+	(typeof client.api.projects)[":projectId"]["$delete"],
+	200
+>;
+type RequestType = InferRequestType<
+	(typeof client.api.projects)[":projectId"]["$delete"]
+>;
+
+export const useUpdateProject = () => {
+	const router = useRouter();
+	const queryClient = useQueryClient();
+	const mutation = useMutation<ResponseType, Error, RequestType>({
+		mutationFn: async ({ param }) => {
+			const response = await client.api.projects[":projectId"].$delete({
+				param,
+			});
+			if (!response.ok) throw new ProjectDeleteError();
+			return await response.json();
+		},
+		onSuccess: ({ data }) => {
+			toast.success("Project deleted");
+			router.refresh();
+			queryClient.invalidateQueries({ queryKey: ["projects"] });
+			queryClient.invalidateQueries({ queryKey: ["project", data.$id] });
+		},
+		onError: () => {
+			toast.error("Failed to delete project");
+		},
+	});
+	return mutation;
+};
