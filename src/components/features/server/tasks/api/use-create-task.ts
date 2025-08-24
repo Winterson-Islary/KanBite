060dd6ddb4490d9893911d98526dcbd062d734c2
Winterson <u19cse1028@cit.ac.in>
2025-08-24 21:48:21 +0530

    add: APIs to get and create tasks

---
+++ src/components/features/server/tasks/api/use-create-task.ts
@@ -0,0 +1,27 @@
+import { client } from "@/lib/rpc";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+import { TaskCreateError } from "../errors/create-task-error";
+
+type ResponseType = InferResponseType<(typeof client.api.tasks)["$post"], 200>;
+type RequestType = InferRequestType<(typeof client.api.tasks)["$post"]>;
+
+export const useCreateTask = () => {
+	const queryClient = useQueryClient();
+	const mutation = useMutation<ResponseType, Error, RequestType>({
+		mutationFn: async ({ json }) => {
+			const response = await client.api.tasks.$post({ json });
+			if (!response.ok) throw new TaskCreateError();
+			return await response.json();
+		},
+		onSuccess: () => {
+			toast.success("Task created");
+			queryClient.invalidateQueries({ queryKey: ["tasks"] });
+		},
+		onError: () => {
+			toast.error("Failed to create task");
+		},
+	});
+	return mutation;
+};
