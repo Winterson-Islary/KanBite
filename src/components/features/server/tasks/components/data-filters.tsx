aec1dd32d70bba339bd53dbfd883cb7c637f463a
Winterson <u19cse1028@cit.ac.in>
2025-08-31 22:24:46 +0530

    add: create a data-filter component

---
+++ src/components/features/server/tasks/components/data-filters.tsx
@@ -0,0 +1,71 @@
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectSeparator,
+	SelectTrigger,
+	SelectValue,
+} from "@/src/components/ui/select";
+import { ListChecksIcon } from "lucide-react";
+import { useGetMembers } from "../../members/api/use-get-members";
+import { useGetProjects } from "../../projects/api/use-get-projects";
+import { useWorkspaceId } from "../../workspaces/hooks/useWorkspaceId";
+import { useTaskFilters } from "../hooks/use-task-filters";
+import { TaskStatus } from "../types/task-status";
+
+interface DataFilterProps {
+	hideProjectFilter?: boolean;
+}
+
+function DataFilters({ hideProjectFilter }: DataFilterProps) {
+	const workspaceId = useWorkspaceId();
+	const { data: projects, isLoading: isLoadingProjects } = useGetProjects({
+		workspaceId,
+	});
+	const { data: members, isLoading: isLoadingMembers } = useGetMembers({
+		workspaceId,
+	});
+	const isLoadingData = isLoadingMembers || isLoadingProjects;
+	const projectOptions = projects?.documents.map((project) => ({
+		value: project.$id,
+		label: project.name,
+	}));
+	const memberOptions = members?.documents.map((member) => ({
+		value: member.$id,
+		label: member.name,
+	}));
+	const [{ status, assigneeId, projectId, search, dueDate }, setFilters] =
+		useTaskFilters();
+	const onStatusChange = (value: string) => {
+		if (value === "all") setFilters({ status: null });
+		else setFilters({ status: value as TaskStatus });
+	};
+	if (isLoadingData) return <article>Loading Data...</article>;
+
+	return (
+		<main className="flex flex-col gap-2 lg:flex-row">
+			<Select
+				defaultValue={status || undefined}
+				onValueChange={(value) => onStatusChange(value)}
+			>
+				<SelectTrigger className="h-8 w-full lg:w-auto">
+					<div className="flex items-center pr-2">
+						<ListChecksIcon className="mr-2 size-4" />
+						<SelectValue placeholder="All statuses" />
+					</div>
+				</SelectTrigger>
+				<SelectContent>
+					<SelectItem value="all">All Statuses</SelectItem>
+					<SelectSeparator />
+					<SelectItem value={TaskStatus.DONE}>Done</SelectItem>
+					<SelectItem value={TaskStatus.PENDING}>Pending</SelectItem>
+					<SelectItem value={TaskStatus.IN_PROGRESS}>In Progress</SelectItem>
+					<SelectItem value={TaskStatus.IN_REVIEW}>In Review</SelectItem>
+					<SelectItem value={TaskStatus.TODO}>Todo</SelectItem>
+				</SelectContent>
+			</Select>
+		</main>
+	);
+}
+
+export default DataFilters;
