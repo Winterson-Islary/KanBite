7d53b46d80f446365af8b5afce6c1f08da3c8de2
Winterson <u19cse1028@cit.ac.in>
2025-09-04 18:49:54 +0530

    add: new Task type

---
+++ src/components/features/server/tasks/types/task.d.ts
@@ -0,0 +1,12 @@
+import type { Models } from "node-appwrite";
+import type { TaskStatus } from "./task-status";
+
+export type Task = Models.Document & {
+	name: string;
+	status: TaskStatus;
+	assigneeId: string;
+	projectId: string;
+	position: number;
+	dueDate: string;
+	description?: string;
+};
