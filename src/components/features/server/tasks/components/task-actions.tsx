70b652c33b654649f0dd64acd953dd761b72811d
Winterson <u19cse1028@cit.ac.in>
2025-09-14 22:29:10 +0530

    modify: add delete mutation to the delete-task button

--- src/components/features/server/tasks/components/task-actions.tsx
+++ src/components/features/server/tasks/components/task-actions.tsx
@@ -4,7 +4,9 @@ import {
 	DropdownMenuItem,
 	DropdownMenuTrigger,
 } from "@/src/components/ui/dropdown-menu";
+import { useConfirm } from "@/src/hooks/useConfirm";
 import { ExternalLinkIcon, PencilIcon, TrashIcon } from "lucide-react";
+import { useDeleteTask } from "../api/use-delete-task";
 
 interface TaskActionsProps {
 	id: string;
@@ -13,14 +15,25 @@ interface TaskActionsProps {
 }
 
 function TaskActions({ id, projectId, children }: TaskActionsProps) {
+	const { mutate: deleteTask, isPending: deleteTaskPending } = useDeleteTask();
+	const [ConfirmDialog, confirm] = useConfirm(
+		"Delete task",
+		"This action cannot be undone",
+	);
+	const onDelete = async () => {
+		const ok = await confirm();
+		if (!ok) return;
+		deleteTask({ param: { taskId: id } });
+	};
+
 	return (
 		<div className="flex justify-end">
+			<ConfirmDialog />
 			<DropdownMenu modal={false}>
 				<DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
 				<DropdownMenuContent align="end" className="w-40">
 					<DropdownMenuItem
 						onClick={() => {}}
-						disabled={false}
 						className="p-[10px] font-medium hover:cursor-pointer"
 					>
 						<ExternalLinkIcon className="mr-2 size-4 stroke-2" />
@@ -28,7 +41,6 @@ function TaskActions({ id, projectId, children }: TaskActionsProps) {
 					</DropdownMenuItem>
 					<DropdownMenuItem
 						onClick={() => {}}
-						disabled={false}
 						className="p-[10px] font-medium hover:cursor-pointer"
 					>
 						<ExternalLinkIcon className="mr-2 size-4 stroke-2" />
@@ -36,15 +48,14 @@ function TaskActions({ id, projectId, children }: TaskActionsProps) {
 					</DropdownMenuItem>
 					<DropdownMenuItem
 						onClick={() => {}}
-						disabled={false}
 						className="p-[10px] font-medium hover:cursor-pointer"
 					>
 						<PencilIcon className="mr-2 size-4 stroke-2" />
 						Edit Task
 					</DropdownMenuItem>
 					<DropdownMenuItem
-						onClick={() => {}}
-						disabled={false}
+						onClick={onDelete}
+						disabled={deleteTaskPending}
 						className="p-[10px] font-medium hover:cursor-pointer"
 					>
 						<TrashIcon className="mr-2 size-4 stroke-2" />
